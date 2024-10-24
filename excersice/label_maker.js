7be768a7ff4097c7c13b47c1ae29043704a8c295
Mehak Sadiq Ali <sadiqalimehak@gmail.com>
2024-10-24 09:46:04 -0400

    wbook4

--- excersice/label_maker.js
+++ excersice/label_maker.js
@@ -1,20 +1,19 @@
-"use strict"
-let myInfo = { 
-    name: "Pursalane Faye",  
-    address: "121 Main Street", 
-    city : "Brooklyn",
-    state : "NY",
-    zipCode : "7272",
-    
+"use strict";
+let myInfo = {
+  name: "Pursalane Faye",
+  address: "121 Main Street",
+  city: "Brooklyn",
+  state: "NY",
+  zipCode: "7272",
 };
 
-    function printPerson(myInfo) {
-        let mailingLabel= `
+function printPerson(myInfo) {
+  let mailingLabel = `
         ${myInfo.name}  
         ${myInfo.address} 
         ${myInfo.city}  
         ${myInfo.state} 
-        ${myInfo.zipCode}  `
-        console.log(mailingLabel)
-    };
-    printPerson(myInfo)
+        ${myInfo.zipCode}  `;
+  console.log(mailingLabel);
+}
+printPerson(myInfo);
